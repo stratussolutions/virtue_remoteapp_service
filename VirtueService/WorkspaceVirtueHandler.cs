5e7b34de7dc15d795642f50b1203c0ce23c59de4
Moser <kamoser@i.cloud1983.us>
2018-04-06 21:29:54 +0100

    fixed impersonation?

--- VirtueService/WorkspaceVirtueHandler.cs
+++ VirtueService/WorkspaceVirtueHandler.cs
@@ -97,8 +97,8 @@ namespace VirtueService
             //WriteLog("Impersonated the virtue domain administrator account.");
             string psscript = @"New-RDSessionCollection -PersonalUnmanaged -CollectionName ""mosertestIE"" -SessionHost ""EC2AMAZ-986SD7H.virtue.local"" -ConnectionBroker ""EC2AMAZ-O3KO101.virtue.local""";
             runPS(psscript);
-            //psscript = @"Add-RDSessionHost -CollectionName ""mosertestIE"" -SessionHost ""EC2AMAZ-986SD7H.virtue.local""";
-            //runPS(psscript);
+            psscript = @"Add-RDSessionHost -CollectionName ""mosertestIE"" -SessionHost ""EC2AMAZ-986SD7H.virtue.local""";
+            runPS(psscript);
             psscript = @"Set-RDPersonalSessionDesktopAssignment -CollectionName ""mosertestIE"" -ConnectionBroker ""EC2AMAZ-O3KO101.virtue.local"" -User ""virtue.local\kamoser"" -Name ""EC2AMAZ-986SD7H.virtue.local""";
             runPS(psscript);
             psscript = @"New-RDRemoteApp -CollectionName ""mosertestIE"" -DisplayName ""InternetExplorer"" -FilePath ""c:/wmp/wmplayer.exe"" -UserGroups ""virtue.local\kamoser""";
